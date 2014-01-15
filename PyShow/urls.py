3bad0569aab612b082e33bde7d98a3d8239a3c8e
JackDeveloper <ei.dobrorodnov@gmail.com>
2014-01-15 16:08:39 +0200

    [users] : create new application 'Users'

--- PyShow/urls.py
+++ PyShow/urls.py
@@ -1,3 +1,4 @@
+import django
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
@@ -6,4 +7,5 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^admin/',    include(admin.site.urls)),
     url(r'^showcase/', include('showcase.urls', namespace = 'showcase')),
+    url(r'^auth/',     include('users.urls',    namespace = 'user')),
 )
